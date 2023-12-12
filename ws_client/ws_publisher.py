04e81e0bb89856465148008ac79a8f2eca1cd108
girotte-tao <taoware@qq.com>
2023-12-12 21:41:50 +0800

    ws publisher using socketio

--- ws_client/ws_publisher.py
+++ ws_client/ws_publisher.py
@@ -1,25 +1,30 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-import websocket
-import json
+import socketio
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
+        timer_period = 1.0  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
-        self.ws = websocket.create_connection("ws://127.0.0.1:5000/")
+
+        self.sio = socketio.Client()
+        self.sio.connect('http://localhost:5000')
 
     def timer_callback(self):
         msg = String()
         msg.data = 'Hello World: %d' % self.i
         self.publisher_.publish(msg)
-        self.ws.send(json.dumps({'data': msg.data}))
+        self.sio.emit('message', msg.data)
         self.i += 1
+
+    def destroy_node(self):
+        self.sio.disconnect()
+        super().destroy_node()
 
 def main(args=None):
     rclpy.init(args=args)
